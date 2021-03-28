77715645d6b53bfa018a37389f2f9e3f46233c41
Ed-Yang <edwardyangyang@hotmail.com>
2021-03-28 15:42:27 +0000

    log gpu failure in kmsg

--- scripts/gpu-failure.sh
+++ scripts/gpu-failure.sh
@@ -6,6 +6,8 @@ sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm"
 # wakeup after 5 minutes
 sh -c "echo `date '+%s' -d '+ 5 minutes'` > /sys/class/rtc/rtc0/wakealarm"
 
+echo "<3>gpuctl: GPU $1 failure, halt and schedule wakeup in 5 minutes" | sudo tee /dev/kmsg 
+
 # halh system and wait boot
 shutdown now
 
